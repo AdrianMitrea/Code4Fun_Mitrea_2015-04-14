73dc01a149cee2ab0a08efbe677ffb6292c1c659
AdrianMitrea <mitreaadi@yahoo.com>
2015-04-16 08:08:44 +0200

    start

---
+++ Code4Fun/Algorithm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Code4Fun
+{
+    public class Algorithm
+    {
+        ///<summary>
+        ///method which calculates the average latency and total bandwidth taking data from headers of TSV files
+        ///</summary>
+        public void Statistics(ref double latency, ref double bandwidth)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UnitTestCode4Fun/UnitTestAlgorithm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Code4Fun;
+
+namespace UnitTestCode4Fun
+{
+    [TestClass]
+    public class UnitTestAlgorithm
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double latency = 0, bandwidth = 0;
+            
+            Algorithm a = new Algorithm();
+
+            a.Statistics(ref latency, ref bandwidth);
+        }
+    }
+}
